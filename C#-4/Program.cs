e9f507fd029bad4c94c68f02c470c657100768cd
Del1-SUDO <kurbonovs397@gmail.com>
2025-01-08 15:05:29 +0500

    task4 not done

---
+++ C#-4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+        int num, cnt=0;
+        for(int i=0; i<n; i++){
+            num=arr[i];
+            cnt=0;
+
+            for(int j=0; j<n; j++){
+                if (num==arr[j]){
+                    cnt++;
+                }
+                if (cnt>=2){
+                    Console.WriteLine(num + " ");
+                }
+            }
+        }
+    }
+}
