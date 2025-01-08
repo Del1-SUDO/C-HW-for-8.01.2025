abead2e2f50dc147116f6a4b06e20b04b3d0dbda
Del1-SUDO <kurbonovs397@gmail.com>
2025-01-08 14:38:29 +0500

    task3 is done

---
+++ C#-3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter all of your numbers in a row: ");
+        int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+        int max=arr[0];
+        int index=0;
+        for (int i=1; i<arr.Length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+                index = i;
+            }
+        }
+
+        Console.WriteLine(max + " " + index);
+    }
+}
